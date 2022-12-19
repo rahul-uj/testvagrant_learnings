84c6f46db78ec30ba9095437b057e70835f3f190
Rahuluj <rahuluj@testvagrant.com>
2022-12-19 12:00:34 +0530

    Sample program

---
+++ GitPractise/Demo.java
@@ -0,0 +1,9 @@
+package collections.GitPractise;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Just a practise program.");
+        System.out.println("Bye");
+    }
+}
